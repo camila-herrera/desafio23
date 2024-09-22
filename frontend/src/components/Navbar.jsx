95d7f0ee9ae8235bfde8bf6c093d3842b2271ab3
camila.herrera <camila.h.gonzalez@gmail.com>
2024-09-22 16:49:57 -0300

    0.2

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
   const { authUser, logOut } = useContext(GlobalContext); 
   const { cartItems } = useCart();
   const total = cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0);
+
   return (
     <nav className='Barra'>
       <div className="btn-group me-2" role="group" aria-label="First group">
@@ -16,7 +17,7 @@ const Navbar = () => {
       </div>
       <div className="d-inline-flex gap-1">
         <NavLink to="/" className="btn active" role="button" style={{ fontSize: 'small' }}>
-          <img src='https://web.whatsapp.com/emoji/v1/15/1/2/single/w/64/01f355.png' className='pizzaimg' alt="Home" />Home
+          <img src='/img/pizza.png' className='pizzaimg' alt="Home" />Home
         </NavLink>
         
         {authUser.token ? (
@@ -25,23 +26,23 @@ const Navbar = () => {
               <img src='/img/candado.png' className='pizzaimg' alt="Profile" />Profile
             </NavLink>
             <button onClick={logOut} className="btn active" role="button" style={{ fontSize: 'small' }}>
-              <img src='candado.png' className='pizzaimg' alt="Logout" />Logout
+              <img src='/img/candado.png' className='pizzaimg' alt="Logout" />Logout
             </button>
           </>
         ) : (
           <>
             <NavLink to="/login" className="btn active" role="button" style={{ fontSize: 'small' }}>
-              <img src='./assets\img\candado(1).png' className='pizzaimg' alt="Login" />Login
+              <img src='/img/candado(1).png' className='pizzaimg' alt="Login" />Login
             </NavLink>
             <NavLink to="/register" className="btn active" role="button" style={{ fontSize: 'small' }}>
-              <img src='candado(1).png' className='pizzaimg' alt="Register" />Register
+              <img src='/img/candado(1).png' className='pizzaimg' alt="Register" />Register
             </NavLink>
           </>
         )}
       </div>
       <div className="d-grid gap-2 d-md-flex justify-content-md-end">
         <NavLink to="/cart" className="btn btn-outline-primary" role="button" style={{ fontSize: 'small' }}>
-          <img src='carrito-de-compras.png' className='pizzaimg' alt="Cart" />Total: ${total}
+          <img src='/img/carrito-de-compras.png' className='pizzaimg' alt="Cart" />Total: ${total}
         </NavLink>
       </div>
     </nav>
